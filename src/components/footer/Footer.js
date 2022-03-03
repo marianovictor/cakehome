36177ce388da01c9f51fe008c62747d91a9c5afa
Victor Rocha <Victor.rocha2002@gmail.com>
2022-03-03 02:11:10 -0300

    Fim estilzacao da home do footer--home/footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./Footer.css"
 import logo from '../../assets/LOGO.png';
-import { BsInstagram } from 'react-icons/bs';
+import { BsInstagram, BsWhatsapp } from 'react-icons/bs';
 import { BsFacebook } from 'react-icons/bs';
 
 export default function Footer() {
@@ -9,54 +9,32 @@ export default function Footer() {
         <div className='container'> 
             <div className='PinkBox'>
                 <div className='upper'>
-                    <div className='title'>
-                        <img src={logo} alt="CACCO"/>
-                        <h1>Centro Acadêmico de Ciências da Computação</h1>
+                    <div className='logo'>
+                        <img src={logo} alt="logo"/>
+                    </div>    
+                    <div className="slogan1">
+                        Quem nao gosta de doces? Por isso fazemos os melhores para voce!
                     </div>
-                    
-                    <div className='column'>
-                        <h4>Endereço</h4>
-                        <ul className='address-details'>
-                            <li>Av. 24 nº 1515,</li>
-                            <li>Bela Vista,</li>
-                            <li>Rio Claro - SP,</li>
-                            <li>IGCE, Unesp.</li>
-                        </ul>
-                        
+                    <div className="contatos">
+                        <a href = "https://www.instagram.com/cakkehome/">
+                            <BsInstagram size={28} color='#000'/>
+                        </a>
                     </div>
-                    
-                    <div className='column'>
-                        <h4>Telefone</h4>
-                        <ul className='phone-numbers'>
-                            <li>(19) 3944-1551</li>
-                        </ul>
+                    <div className="contatos">
+                        <a href = "https://www.facebook.com/cakkehome">    
+                            <BsFacebook size={28} color='#000'/>
+                        </a>
                     </div>
-                    
-                    <div className='column'>
-                        <h4>Contato</h4>
-                        <ul>
-                            <a href = "https://www.w3schools.com/">email.com.br</a>
-                            <br/>
-                            <div className='icons'>   
-                            <a href = "https://www.w3schools.com/">
-                                <BsInstagram size={30} color='#000'/>
-                            </a>
-                            </div>
-                            <div className='icons'>
-                            <a href = "https://www.w3schools.com/">    
-                                <BsFacebook size={30} color='#000'/>
-                            </a>
-                            </div>
-                        </ul>
-                    </div>
-
+                    <div className="contatos">
+                        <a href = "https://wa.me/5519992678295">    
+                             <BsWhatsapp size={28} color='#000'/>
+                        </a>
+                    </div>                
                 </div>
-
                 <div className='lower'>
-                    <div className="bottomCACCo">
-                        <h3>CACCo &copy;{new Date().getFullYear()}</h3>
-                        <h3>Unesp Rio Claro</h3>
-                        <h3>Desenvolvido por <a href="https://infojrunesp.com.br/">InfoJr</a></h3>
+                    <div className="direitos">
+                        <h3>Powered By <a href="https://www.linkedin.com/in/victor-rocha-8b39ba21a/">Victor Mariano Rocha</a></h3>
+                        <h3>&copy;{new Date().getFullYear()} Cake Home Todos os direitos reservados</h3>
                     </div>
                 </div>             
             </div>  
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,6 +4,8 @@ import Carroseul, { CarouselItem }from '../../components/carroseul/carrouseul';
 import logo from '../../assets/LogoRedondo.png';
 import foto1 from '../../assets/testeFoto.jpg';
 import Fade from 'react-reveal/Fade';
+import { BsInstagram, BsWhatsapp } from 'react-icons/bs';
+import { BsFacebook } from 'react-icons/bs';
 
 export default function Home() {
     return(
@@ -31,9 +33,7 @@ export default function Home() {
                         <p>Esse voluptate id culpa culpa excepteur aute. Sunt reprehenderit deserunt sit cupidatat aute aliqua irure aute reprehenderit do veniam aliquip occaecat magna. Anim labore anim esse elit officia anim id veniam irure enim. Lorem anim sunt id cillum sit deserunt excepteur minim ipsum ut fugiat aute. Ad non irure aute sunt officia nisi duis tempor. Labore pariatur magna aute ad sunt.</p>
             </div>
             </div>
-           
-                <div className="PhotosContainer">
-                    
+                <div className="PhotosContainer">  
                     <div className="Photos">
                             <Carroseul>
                                 <CarouselItem><img src={foto1} alt="foto"/></CarouselItem>
@@ -41,7 +41,36 @@ export default function Home() {
                                 <CarouselItem><img src={foto1} alt="foto"/></CarouselItem>
                             </Carroseul>
                     </div>
-                    
+                </div>
+            </Fade>
+            <Fade right>
+            <div className="titleAbout">
+                <h1>Cardapio</h1>
+            </div>
+                <div className="aboutUsContainer">
+                    <div className="textAbout">
+                        <p>Esse voluptate id culpa culpa excepteur aute. Sunt reprehenderit deserunt sit cupidatat aute aliqua irure aute reprehenderit do veniam aliquip occaecat magna. Anim labore anim esse elit officia anim id veniam irure enim. Lorem anim sunt id cillum sit deserunt excepteur minim ipsum ut fugiat aute. Ad non irure aute sunt officia nisi duis tempor. Labore pariatur magna aute ad sunt.</p>
+                        <img src={logo} alt="logo"/>
+                    </div>
+                </div>
+            </Fade>
+            <Fade left>
+            <div className="titleContato">
+                <h1>Contato</h1>
+            </div>
+                <div className="ContatoContainer">
+                    <div className="textContato">
+                    <div className="icons">
+                        <a href = "https://www.instagram.com/cakkehome/"><BsInstagram size={28} color='#000'/>@cakkehome</a>
+                        <br/>
+                        <br/>
+                        <a href = "https://www.facebook.com/cakkehome"><BsFacebook size={28} color='#000'/>cakkehome</a>
+                        <br/>
+                        <br/>
+                        <a href = "https://wa.me/5519992678295"><BsWhatsapp size={28} color='#000'/>(19) 99267-8295</a>
+                    </div>
+                        <p>Esse voluptate id culpa culpa excepteur aute. Sunt reprehenderit deserunt sit cupidatat aute aliqua irure aute reprehenderit do veniam aliquip occaecat magna. Anim labore anim esse elit officia anim id veniam irure enim. Lorem anim sunt id cillum sit deserunt excepteur minim ipsum ut fugiat aute. Ad non irure aute sunt officia nisi duis tempor. Labore pariatur magna aute ad sunt.</p>
+                    </div>
                 </div>
             </Fade>
         </div>
