d118b004590203d61e4435045d7ccbe231b484ad
Victor Rocha <Victor.rocha2002@gmail.com>
2022-03-03 00:38:27 -0300

    arrumando card 'fotos'--home

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,9 +2,7 @@ import React from 'react';
 import './home.css';
 import Carroseul, { CarouselItem }from '../../components/carroseul/carrouseul';
 import logo from '../../assets/LogoRedondo.png';
-import foto1 from '../../assets/foto1.png';
-import foto2 from '../../assets/foto2.png';
-import foto3 from '../../assets/foto3.png';
+import foto1 from '../../assets/testeFoto.jpg';
 import Fade from 'react-reveal/Fade';
 
 export default function Home() {
@@ -29,16 +27,21 @@ export default function Home() {
             <Fade left>
             <div className="titlePhotos">
                 <h1>Fotos</h1>
-            </div>
-                <div className="PhotosContainer">
-                    <div className="textPhotos">
-                        <Carroseul>
-                            <CarouselItem><img src={foto1} alt="foto"/></CarouselItem>
-                            <CarouselItem><img src={foto2} alt="foto"/></CarouselItem>
-                            <CarouselItem><img src={foto3} alt="foto"/></CarouselItem>
-                        </Carroseul>
+                <div className="textPhotos">
                         <p>Esse voluptate id culpa culpa excepteur aute. Sunt reprehenderit deserunt sit cupidatat aute aliqua irure aute reprehenderit do veniam aliquip occaecat magna. Anim labore anim esse elit officia anim id veniam irure enim. Lorem anim sunt id cillum sit deserunt excepteur minim ipsum ut fugiat aute. Ad non irure aute sunt officia nisi duis tempor. Labore pariatur magna aute ad sunt.</p>
+            </div>
+            </div>
+           
+                <div className="PhotosContainer">
+                    
+                    <div className="Photos">
+                            <Carroseul>
+                                <CarouselItem><img src={foto1} alt="foto"/></CarouselItem>
+                                <CarouselItem><img src={foto1} alt="foto"/></CarouselItem>
+                                <CarouselItem><img src={foto1} alt="foto"/></CarouselItem>
+                            </Carroseul>
                     </div>
+                    
                 </div>
             </Fade>
         </div>
