39be2785cefb68d6332f9c141dc49bf02e885657
Victor Rocha <Victor.rocha2002@gmail.com>
2022-03-07 14:55:26 -0300

    responsividade tela home e footer-home/-footer

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -33,9 +33,9 @@ export default function Home() {
             <Fade left>
             <div className="titlePhotos">
                 <h1><Link to='/Fotos' style={{ color: 'var(--color-text)' }}>Fotos</Link></h1>
-                <div className="textPhotos">
-                        <p>Esse voluptate id culpa culpa excepteur aute. Sunt reprehenderit deserunt sit cupidatat aute aliqua irure aute reprehenderit do veniam aliquip occaecat magna. Anim labore anim esse elit officia anim id veniam irure enim. Lorem anim sunt id cillum sit deserunt excepteur minim ipsum ut fugiat aute. Ad non irure aute sunt officia nisi duis tempor. Labore pariatur magna aute ad sunt.</p>
             </div>
+            <div className="textPhotos">
+                <p>Esse voluptate id culpa culpa excepteur aute. Sunt reprehenderit deserunt sit cupidatat aute aliqua irure aute reprehenderit do veniam aliquip occaecat magna. Anim labore anim esse elit officia anim id veniam irure enim. Lorem anim sunt id cillum sit deserunt excepteur minim ipsum ut fugiat aute. Ad non irure aute sunt officia nisi duis tempor. Labore pariatur magna aute ad sunt.</p>
             </div>
                 <div className="PhotosContainer">  
                     <div className="Photos">
